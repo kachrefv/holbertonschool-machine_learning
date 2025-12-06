4c3dbdc0783b9db2662d79287d4b5815e71f50ac
kachrefv <achrefr556@gmail.com>
2025-12-06 16:29:15 +0100

    Update 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
-    shape = [] #matrix shape
+    shape = []  #matrix shape
     m = matrix
     while isinstance(m, list):
         shape.append(len(m))
         if len(m) == 0:
             break
         m = m[0]
-    return shape #return shape
+    return shape  #return shape
