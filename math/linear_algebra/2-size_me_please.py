7a76106af0517a4622876bc2506c4bea2532264e
kachrefv <achrefr556@gmail.com>
2025-12-06 16:19:06 +0100

    Create 2-size_me_please.py

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def matrix_shape(matrix):
+    shape = []
+    m = matrix
+    while isinstance(m, list):
+        shape.append(len(m))
+        if len(m) == 0:
+            break
+        m = m[0]
+    return shape
