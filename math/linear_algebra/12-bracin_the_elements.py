44bb12590c7670b1c71def915ac81b9a44f22fd4
kachrefv <achrefr556@gmail.com>
2025-12-07 18:14:49 +0100

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -5,8 +5,6 @@ The module provides a single function `np_elementwise` which returns
 a tuple of element-wise sum, difference, product, and quotient.
 """
 
-import numpy as np
-
 
 def np_elementwise(mat1, mat2):
     """Return element-wise operations on two arrays.
@@ -18,6 +16,4 @@ def np_elementwise(mat1, mat2):
     Returns:
         A tuple containing (sum, difference, product, quotient) arrays.
     """
-    mat1 = np.asarray(mat1)
-    mat2 = np.asarray(mat2)
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
