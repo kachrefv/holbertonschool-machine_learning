a38db52696236d9cad28a7de7626a7749d69e7f6
kachrefv <achrefr556@gmail.com>
2025-12-27 22:26:03 +0100

    Create 1-build_decision_tree.py

---
+++ supervised_learning/decision_tree/1-build_decision_tree.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""Module to build a decision tree"""
+
+import numpy as np
+
+
+class Node:
+    """Class that represents a node in a decision tree"""
+    def __init__(self, feature=None, threshold=None, left_child=None,
+                 right_child=None, is_root=False, depth=0):
+        """Initializes a node"""
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Returns the maximum depth below the node"""
+        return max(self.left_child.max_depth_below(),
+                   self.right_child.max_depth_below())
+
+    def count_nodes_below(self, only_leaves=False):
+        """Counts the number of nodes below the current node"""
+        if only_leaves:
+            return (self.left_child.count_nodes_below(only_leaves=True) +
+                    self.right_child.count_nodes_below(only_leaves=True))
+        return (1 + self.left_child.count_nodes_below(only_leaves=False) +
+                self.right_child.count_nodes_below(only_leaves=False))
+
+
+class Leaf(Node):
+    """Class that represents a leaf in a decision tree"""
+    def __init__(self, value, depth=None):
+        """Initializes a leaf"""
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Returns the maximum depth below the leaf"""
+        return self.depth
+
+    def count_nodes_below(self, only_leaves=False):
+        """Counts the number of nodes below (only 1 for leaf)"""
+        return 1
+
+
+class Decision_Tree():
+    """Class that represents a decision tree"""
+    def __init__(self, max_depth=10, min_pop=1, seed=0,
+                 split_criterion="random", root=None):
+        """Initializes a decision tree"""
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        """Returns the maximum depth of the tree"""
+        return self.root.max_depth_below()
+
+    def count_nodes(self, only_leaves=False):
+        """Counts the number of nodes in the tree"""
+        return self.root.count_nodes_below(only_leaves=only_leaves)
